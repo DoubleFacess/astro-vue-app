5604ea1f3d3f2d6d93ddb70a720f369dd34ff963
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2024-04-24 13:13:20 +0200

    fixing version

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,54 +18,7 @@
 </template>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  width: 600px;
-  height: 500px;
-  justify-content: center; /* Centra orizzontalmente */
-  align-items: center; /* Centra verticalmente */
-  height: 100vh; /* Altezza del contenitore uguale all'altezza della finestra del browser */
-  margin-left: auto;
-  margin-right: auto;
-}
 
-.form-group {
-  margin-bottom: 10px;
-}
-
-label {
-  display: block;
-}
-
-input[type="text"],
-input[type="password"] {
-  width: 100%;
-  padding: 5px;
-}
-
-button[type="submit"] {
-  padding: 10px 20px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  cursor: pointer;
-}
-
-button[type="submit"]:hover {
-  background-color: #45a049;
-}
-
-
-@import url('https://fonts.googleapis.com/css2?family=Work+Sans&display=swap');
-
-.bg-btn {
-  background-color: #3C9521;
-}
-
-.container {
-  font-family: 'Work Sans', sans-serif;
-}
 </style>
     
 
